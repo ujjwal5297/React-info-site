4ef77d2bea1cf535bae13a2bcc7b9a5d922bc1b9
ujjwal5297 <ujjwalsharma5297@gmail.com>
2022-07-26 23:21:56 +0530

    Updates

---
+++ precache-manifest.ae6950f5507e1b674d73d8cd545c8f84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9cb0ed8994592f1f648e",
+    "url": "/React-info-site/static/css/main.e5c84675.chunk.css"
+  },
+  {
+    "revision": "9cb0ed8994592f1f648e",
+    "url": "/React-info-site/static/js/main.9cb0ed89.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/React-info-site/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "fb9cd26e443447d12d2e",
+    "url": "/React-info-site/static/js/2.fb9cd26e.chunk.js"
+  },
+  {
+    "revision": "7428339b584ecbd0e5f0",
+    "url": "/React-info-site/static/js/runtime~main.7428339b.js"
+  },
+  {
+    "revision": "3cf817075ffdc798cf13c457bf4c3bc5",
+    "url": "/React-info-site/static/media/logo.3cf81707.png"
+  },
+  {
+    "revision": "468de78e1a900e3de9ac5997b7492ce8",
+    "url": "/React-info-site/index.html"
+  }
+];
